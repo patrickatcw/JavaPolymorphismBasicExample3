fb05ec5cbe3b78ba8f1025169d6c40d99addb83b
patrickatcw <pj8392@yahoo.com>
2018-05-15 10:52:13 -0400

    horse class with override method

---
+++ src/com/me/Horse.java
@@ -0,0 +1,13 @@
+package com.me;
+
+//step 3 make this class
+//step 4 extends Animal
+public class Horse extends Animal{
+
+    //step 5 override method
+    @Override
+    public void sound(){
+        System.out.println("Neigh");
+    }
+
+}
