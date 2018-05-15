c41868eb232633ffefe99e15ae2cddd150610302
patrickatcw <pj8392@yahoo.com>
2018-05-15 10:46:58 -0400

    animal class made with method

---
+++ src/com/me/Animal.java
@@ -0,0 +1,11 @@
+package com.me;
+
+//step 1 make this class
+public class Animal {
+
+    //step 2 method
+    public void sound(){
+        System.out.println("Animal is making a sound");
+    }
+
+}
