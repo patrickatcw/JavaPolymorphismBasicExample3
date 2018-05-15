d02e820450f9d23fcb8d7ac91da058116ce198fa
patrickatcw <pj8392@yahoo.com>
2018-05-15 10:54:23 -0400

    cat class with override method

---
+++ src/com/me/Cat.java
@@ -0,0 +1,13 @@
+package com.me;
+
+//step 6 make this class
+//step 7 extends animal
+public class Cat extends Animal{
+
+    //step 8 override method
+    @Override
+    public void sound(){
+        System.out.println("Meow");
+    }
+
+}
